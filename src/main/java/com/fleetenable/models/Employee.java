0959a22c8e8685838e8b110d17b74f1b1392bb61
rkjyothi1997 <rkjyothi1997@gmail.com>
2024-03-22 17:57:48 +0530

    added model employee classes
    added model employee classes

---
+++ src/main/java/com/fleetenable/models/Employee.java
@@ -0,0 +1,52 @@
+package com.fleetenable.models;
+
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank
+	private String firstName;
+
+	@NotBlank
+	private String lastName;
+
+	@Email
+	private String email;
+
+	@ElementCollection
+	@CollectionTable(name = "employee_phone_numbers", joinColumns = @JoinColumn(name = "employee_id"))
+	@Column(name = "phone_number")
+	private List<String> phoneNumbers;
+
+	private LocalDate doj;
+
+	@Positive
+	private Double salary;
+
+}
