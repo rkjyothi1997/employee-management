32a01e80dd107995daa7aefdbdafb12974a44e38
rkjyothi1997 <rkjyothi1997@gmail.com>
2024-03-22 18:38:34 +0530

    final implementation done
    final implementation done

--- src/main/java/com/fleetenable/service/EmployeeService.java
+++ src/main/java/com/fleetenable/service/EmployeeService.java
@@ -11,18 +11,27 @@ import com.fleetenable.utils.EmployeeUtil;
 public class EmployeeService {
 
 	public TaxDetails getTaxDetails(Employee employee) {
+
 		LocalDate startOfFinancialYear = LocalDate.of(LocalDate.now().getYear(), Month.APRIL, 1);
 		if (employee.getDoj().isAfter(startOfFinancialYear)) {
 			startOfFinancialYear = employee.getDoj();
 		}
 
-		long monthsWorked = ChronoUnit.MONTHS.between(startOfFinancialYear, LocalDate.now()) + 1;
+		long monthsWorked = ChronoUnit.MONTHS.between(startOfFinancialYear.withDayOfMonth(1), LocalDate.now()) + 1;
 		double totalSalary = employee.getSalary() * monthsWorked;
+
+		if (employee.getDoj().getDayOfMonth() > 1) {
+			double dailyRate = employee.getSalary() / 30;
+			int daysToDeduct = employee.getDoj().getDayOfMonth() - 1;
+			totalSalary -= dailyRate * daysToDeduct;
+		}
+
 		double taxAmount = EmployeeUtil.calculateTax(totalSalary);
 		double cessAmount = totalSalary > 2500000 ? (totalSalary - 2500000) * 0.02 : 0;
 
 		TaxDetails taxDetails = new TaxDetails(employee.getId(), employee.getFirstName(), employee.getLastName(),
 				totalSalary, taxAmount, cessAmount);
+
 		return taxDetails;
 	}
 
